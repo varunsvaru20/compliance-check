dd11d31ab1a54e9dc42ef8e247734806fbbb8511
varunsvaru20 <108314608+varunsvaru20@users.noreply.github.com>
2025-01-20 17:58:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='compliance-check',
-    version='0.1',
+    version='0.2',
     packages=['compliance_checker'],
     include_package_data=True,
     zip_safe=False
